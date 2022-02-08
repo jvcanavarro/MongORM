8e667c8b5301d34df2d0b36549586b17aae98a3a
João Canavarro <jvcanavarro@gmail.com>
2022-02-07 21:53:38 -0300

    Update movie model with genres and awards fields

--- source/model.py
+++ source/model.py
@@ -1,10 +1,11 @@
 from mongoengine import DynamicDocument
-from mongoengine.fields import StringField, IntField, ListField
+from mongoengine.fields import StringField, IntField, ListField, DictField
 
 
 class MovieModel(DynamicDocument):
-    fullplot = StringField()
     genres = ListField()
+    fullplot = StringField()
+    awards = DictField()
     runtime = IntField()
     title = StringField()
     meta = {"collection": "movies"}
