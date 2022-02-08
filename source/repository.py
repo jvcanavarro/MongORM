15de160920d783e7c0d60d48b74bc0a8e6f508fa
João Canavarro <jvcanavarro@gmail.com>
2022-02-07 21:53:54 -0300

    Add 2 new queries with List and DictField operations

--- source/repository.py
+++ source/repository.py
@@ -7,3 +7,9 @@ class MovieRepository:
 
     def get_movie_by_id(self, id):
         return MovieModel.objects(id=id).first()
+
+    def get_movies_by_genre(self, genre):
+        return MovieModel.objects(genres=genre)
+
+    def get_movies_by_awards(self, n_awards):
+        return MovieModel.objects(awards__wins=int(n_awards))
